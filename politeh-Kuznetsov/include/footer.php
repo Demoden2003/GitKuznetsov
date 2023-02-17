4a6c6497252d1d7efc2a77dd67b437f7b431f065
Demoden2003 <124438615+Demoden2003@users.noreply.github.com>
2023-02-17 23:38:28 +0300

    Add files via upload

---
+++ politeh-Kuznetsov/include/footer.php
@@ -0,0 +1,126 @@
+<body>
+    <div class = glav>
+        <img src = "./assets/img/Призрак.png" width="70px" height="70px">
+        <button class = "styleb"><p class = "buttong">О нас</p></button>
+        <button class = "styleb"><p class = "buttong">Преймущество</p></button>
+        <button class = "styleb"><p class = "buttong">Цены</p></button>
+        <button class = "styleb"><p class = "buttong">Акции</p></button>
+        <button class = "styleb"><p class = "buttong">Отзывы</p></button>
+        <button class = "styleb"><p class = "buttong">Контакты</p></button>
+        <button class = "stylevhod">Вход</button>
+        <button class = "stylereg">Регистрация</button>
+    </div>
+    <div class = "lol">
+        <div class = "menu">
+            <ul>
+                <li>Завтраки</li>
+                <li>Первые блюда</li>
+                <li>Вторые блюда</li>
+                <li>Десерты</li>
+                <li>Выпечка</li>
+                <li>Соусы</li>
+                <li>Напитки</li>
+                <li>Закуски</li>
+                <li>Заготовки</li>
+            </ul>
+            <hr>
+            <ul>
+                <li>О посуде</li>
+                <li>Лайфхаки</li>
+                <li>Статьи</li>
+            </ul>
+            <hr>
+            <div class = "paint">
+                <img src = "./assets/img/Vk_icon.png" width="20px" height="20px">
+                <img src = "./assets/img/fb_icon.png" width="20px" height="20px">
+                <img src = "./assets/img/in_icon.png" width="20px" height="20px">
+            </div>
+        </div>
+        <div class = "normal">
+            <b class = "p1">Best Resiper</b>
+            <div class = "p2">рецепты на любой случай</div>
+            <hr>
+            <div class = "lol">
+                <div class="blin">
+                    <img src="./assets/img/Блины.png" width="520" height="360">
+                </div>
+                <div class="glavtext">
+                    <div>Главная/Завтраки/Сырники</div>
+                    <div class = "textzag">Творожные сырники<br>
+                        с бананом и шоколадом</div>
+                    <div>Необычный вариант десерта, который особенно<br>
+                    оценят сладкоежки - сырники с бананом и<br>
+                    шоколадом. Растаявший шоколад внутри и<br>
+                    румянная корочка - это невероятно вкусно,<br>
+                    а также очень быстро в приготовлении! 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+            <div class = "fond">
+                <div>
+                    <div class = "sibir"><img src = "./assets/img/sberbank_icon.png" class = "pg" width="35px" height="35px"><div class = "text-sibir">Сибирь банк</div></div>
+                    <div class = "number">8 800 999 55 55</div>
+                    <div class = "russ">Звонок по россии бесплатно</div>
+                </div>
+                <div>
+                    <div class = "text-down">
+                        Банк
+                    </div>
+                    <ul>
+                        <li>О банке</li>
+                        <li>Новости</li>
+                        <li>Курс валют</li>
+                        <li>Вакансии</li>
+                        <li>Банкоматы</li>
+                        <li>Контакты</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class = "text-down">
+                        Карты
+                    </div>
+                    <ul>
+                        <li>Кредитная</li>
+                        <li>Депитовная</li>
+                        <li>Зарплатная</li>
+                        <li>Пенсионная</li>
+                        <li>Социальная</li>
+                        <li>Молодежная</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class = "text-down">
+                        Карты
+                    </div>
+                    <ul>
+                        <li>Инвенстиции</li>
+                        <li>Брокарский счет</li>
+                        <li>Облигации</li>
+                        <li>Паевые фонды</li>
+                        <li>Управление</li>
+                        <li>Трейдинг</li>
+                        <li>Обучение</li>
+                    </ul>
+                </div>
+                <div>
+                    <div class = "text-down">
+                        Ипотека
+                    </div>
+                    <ul>
+                        <li>Новостройки</li>
+                        <li>Вторичное жилье</li>
+                        <li>Рефинансирование</li>
+                        <li>Для госслужащих</li>
+                        <li>Гос поддержка</li>
+                        <li>Ипотечный калькулятор</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class = "down">
+                <div>2020 @ПАО Сибирь банк</div>
+                <div>Политика конфиденциальности</div>
+            </div>
+</body>
